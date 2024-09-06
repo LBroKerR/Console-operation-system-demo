690b87e2e7f7c35159315732bcfeb8de52a6634d
LBroKerR <144912085+LBroKerR@users.noreply.github.com>
2024-09-06 14:49:16 +0200

    Update MenuFunctions.h

--- SerialTask/MenuFunctions.h
+++ SerialTask/MenuFunctions.h
@@ -8,20 +8,20 @@ using namespace std;
 #define EEPROM_SIZE  0x0400 // 1024 bytes
 
 #define FPROG1	     0x0000	// daily programs addresses
-#define FPROG2		 0x00C0	// contain 24 double data/ daily program. 
+#define FPROG2		   0x00C0	// contain 24 double data/ daily program. 
 #define FPROG3	     0x0180
 #define FPROG4	     0x0240
 #define FPROG5	     0x0301
 
-#define USER	     0x03C0	// wifi data addresses
-#define PW		     0x03C8
-#define IP		     0x03D0
-#define HOST	     0x03E0
+#define USER	       0x03C0	// wifi data addresses
+#define PW		       0x03C8
+#define IP		       0x03D0
+#define HOST	       0x03E0
 #define ENABLE	     0x03E4
 
-#define HOUR	     0x03E5	// other data addresses
-#define MIN			 0x03E9
-#define PROG	     0x03ED	//active prog
+#define HOUR	       0x03E5	// other data addresses
+#define MIN			     0x03E9
+#define PROG	       0x03ED	//active prog
 #define TMPOFFSET    0x03F1
 
 bool hardreset(void* param);
